a0cbe140b585636c2750a1b0bfa90e23a2217cd3
Nahuel Albarracin <nahuel.info27@gmail.com>
2022-10-15 01:14:02 -0300

    add HashRouter

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { ForecastProvider } from './context/ForecastContext';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ForecastProvider } from "./context/ForecastContext";
+import { HashRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-   <ForecastProvider>
-    <App />
-   </ForecastProvider>
-    </BrowserRouter>
+    <HashRouter>
+      <ForecastProvider>
+        <App />
+      </ForecastProvider>
+    </HashRouter>
   </React.StrictMode>
 );
 
